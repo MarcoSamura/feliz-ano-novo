cbcfe4613dbee49570c1ee9d3184b03dde90aacf
MarcoSamuel <marcossamuelsin@gmail.com>
2021-01-02 15:48:56 -0300

    Projeto de ano novo

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const animationLetter = {
+	animated: document.querySelector('#oldYear'),
+
+	initAnimated() {
+		this.animated.addEventListener('animationend', () => {
+		  console.log('Feliz ano novo');
+		  animated.innerHTML = '1'
+		});
+	}
+}
+
+const backgroundAnimation = {
+	initAnimated() {
+		VANTA.WAVES({
+		  el: "html",
+		  mouseControls: true,
+		  touchControls: true,
+		  gyroControls: true,
+		  minHeight: 200.00,
+		  minWidth: 200.00,
+		  scale: 1.00,
+		  scaleMobile: 1.00,
+		  color:0x000000		
+		})
+	}
+}
+backgroundAnimation.initAnimated()
+animationLetter.initAnimated();
